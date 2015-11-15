d145ceea975717e18acc56d85636a6855e88244b
Sven Perkmann <sven.perkmann@klarna.com>
2015-11-15 09:41:32 +0100

    removing temporary SSL support, will be implemented with reverse nginx proxy instead

--- server/server.js
+++ server/server.js
@@ -5,11 +5,3 @@ Meteor.startup(function() {
     scheduleJob(job._id, job.schedule);
   });
 });
-
-
-// SSL should be handled by nginx eventually, but for now nourharidy:ssl should do the trick.
-// As port 443 requires root permissions I'm only activating the SSL proxy server for the root user.
-if (process.env.USER === 'root') {
-  const basePath = process.env.PWD;
-  SSL(`${basePath}/private/server.key`,`${basePath}/private/server.crt`, 443);
-}
