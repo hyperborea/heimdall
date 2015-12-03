37a057c4d11b60a9f8cdf53251c7e8b5d400615c
Sven Perkmann <sven.perkmann@klarna.com>
2015-12-03 10:35:39 +0100

    fixing form action visibility

--- client/views/_helpers/permissionHelpers.js
+++ client/views/_helpers/permissionHelpers.js
@@ -5,3 +5,7 @@ Template.registerHelper('isOwner', function(doc) {
 Template.registerHelper('ownerVisibility', function(doc) {
   return isOwner(Meteor.user(), doc) ? 'visible' : 'hidden';
 });
+
+Template.registerHelper('ownerOrNewVisibility', function(doc) {
+  return !doc || isOwner(Meteor.user(), doc) ? 'visible' : 'hidden';
+});
