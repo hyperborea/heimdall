98f40472f1fda54f531d48fdddd9e08f8ab0c3ff
Sven Perkmann <sven.perkmann@klarna.com>
2016-04-11 22:45:28 +0200

    rerunning zombie jobs on server restart

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 Meteor.startup(function() {
   SyncedCron.start();
 
+  // mark previously running jobs as zombies
+  Jobs.update({status: 'running'}, {$set: {status: 'zombie'}}, {multi: true});
+
+  // reschedule jobs
   Jobs.find({schedule: {$ne: ''}}).forEach((job) => {
     scheduleJob(job._id, job.schedule);
+  });
+
+  Jobs.find({status: 'zombie'}).forEach((job) => {
+    console.warn(`Rerunning zombie job "${job._id}"`);
+    runJob(job._id);
   });
 });
 
