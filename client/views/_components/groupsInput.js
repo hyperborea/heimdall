204cdc2aba90d174150bddc68dbabee0275c5c7b
Sven Perkmann <sven.perkmann@klarna.com>
2016-06-05 19:21:57 +0200

    publishing groups for groupsinput lazily

--- client/views/_components/groupsInput.js
+++ client/views/_components/groupsInput.js
@@ -1,7 +1,19 @@
+Template.groupsInput.onCreated(function() {
+  this.search = new ReactiveVar();
+
+  this.autorun(() => {
+    this.subscribe('groups', this.search.get());
+  });
+});
+
+
 Template.groupsInput.onRendered(function() {
-  this.subscribe('groups', () => {
-    Tracker.afterFlush(() => {
-      this.$('.ui.dropdown').dropdown();
+  this.autorun(() => {
+    Template.currentData();
+
+    this.$('.ui.dropdown').dropdown({
+      allowAdditions: true,
+      fullTextSearch: true
     });
   });
 });
@@ -9,4 +21,11 @@ Template.groupsInput.onRendered(function() {
 
 Template.groupsInput.helpers({
   groups: () => Groups.find()
+});
+
+
+Template.groupsInput.events({
+  'keyup input.search': function(event, template) {
+    template.search.set(event.target.value);
+  }
 });
