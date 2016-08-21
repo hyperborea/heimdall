5815eb42b2cefaa7f1e1dedc1f2d31d08b6515bc
Sven Perkmann <sven.perkmann@klarna.com>
2016-08-21 19:32:28 +0200

    tricking gridster not to move widgets down if they don't fit

--- client/views/dashboard/form.js
+++ client/views/dashboard/form.js
@@ -19,6 +19,7 @@ Template.dashboardForm.onRendered(function() {
   var grid = this.$('.gridster').gridster({
     widget_margins: [10, 10],
     widget_base_dimensions: [80, 60],
+    min_cols: 10,
     max_size_x: 20,
     resize: {
       enabled: true
