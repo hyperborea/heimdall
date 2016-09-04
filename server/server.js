078792866c0014082b40d07e036935b498986fc2
Sven Perkmann <sven.perkmann@klarna.com>
2016-09-04 11:43:42 +0200

    supporting multicore clustering through meteorhacks:cluster

--- server/server.js
+++ server/server.js
@@ -2,20 +2,23 @@ _.extend(LDAP_SETTINGS, Meteor.settings.ldap);
 
 
 Meteor.startup(function() {
-  SyncedCron.start();
+  // only run cronjobs on master node if using multicore clustering
+  if (!process.env.CLUSTER_WORKER_ID) {
+    SyncedCron.start();
 
-  // mark previously running jobs as zombies
-  Jobs.update({status: 'running'}, {$set: {status: 'zombie'}}, {multi: true});
+    // mark previously running jobs as zombies
+    Jobs.update({status: 'running'}, {$set: {status: 'zombie'}}, {multi: true});
 
-  // reschedule jobs
-  Jobs.find({schedule: {$ne: ''}}).forEach((job) => {
-    scheduleJob(job._id, job.schedule);
-  });
+    // reschedule jobs
+    Jobs.find({schedule: {$ne: ''}}).forEach((job) => {
+      scheduleJob(job._id, job.schedule);
+    });
 
-  Jobs.find({status: 'zombie'}).forEach((job) => {
-    console.warn(`Rerunning zombie job "${job._id}"`);
-    runJob(job._id);
-  });
+    Jobs.find({status: 'zombie'}).forEach((job) => {
+      console.warn(`Rerunning zombie job "${job._id}"`);
+      runJob(job._id);
+    });
+  }
 });
 
 
