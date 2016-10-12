f9cd72fd631e91701c357d44c2e9b9266cbd2ac1
Sven Perkmann <sven.perkmann@klarna.com>
2016-10-12 22:11:17 +0200

    making value format for bigNumbers configurable

--- client/views/visualization/widgets/bigNumber.js
+++ client/views/visualization/widgets/bigNumber.js
@@ -3,14 +3,15 @@ import { format as d3Format } from 'd3';
 
 Template.visBigNumber.helpers({
   value: function() {
-    const settings = this.settings;
+    var settings = this.settings;
+    settings.valueFormat = settings.valueFormat || ',';
 
     if (settings.valueField && settings.valueAggregation) {
       var aggregationFunction = AGGREGATIONS[settings.valueAggregation].fn;
       var values = _.pluck(this.data, settings.valueField);
       var result = aggregationFunction(values);
 
-      return _.isNumber(result) ? d3Format(',')(result) : result;
+      return _.isNumber(result) ? d3Format(settings.valueFormat)(result) : result;
     }
     else return '-';
   },
