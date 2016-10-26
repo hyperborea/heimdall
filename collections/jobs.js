8fcc5d7c5045824ca62b5c74d2259940a9f9d96a
Sven Perkmann <sven.perkmann@klarna.com>
2016-10-26 09:55:39 +0200

    removing "disconnect" feature as well

--- collections/jobs.js
+++ collections/jobs.js
@@ -219,9 +219,11 @@ runJob = function(jobId, parameters) {
 
   function updateJob(result, firstUpdate=false) {
     // the job might have been rerun in a different thread by now, don't update in that case
-    if (!firstUpdate && job.result(parameters).runId !== runId) {
-      console.warn(`Run ${runId} for job ${jobId} has been disconnected, not updating results`);
-      return;
+    var jobRunId = job.result(parameters).runId;
+    if (!firstUpdate && jobRunId !== runId) {
+      console.log(`Job ${jobId} disconnected? ${jobRunId} != ${runId}`);
+      // console.warn(`Run ${runId} for job ${jobId} has been disconnected, not updating results`);
+      // return;
     }
 
     // only update job status if parameteres are the job defaults
