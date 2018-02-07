b8b3d00e55a1d623c867318cfafdf84144ecdbaf
Sven <sven.perkmann@gmail.com>
2018-02-07 16:30:38 +0100

    custom users are always using username, not email

--- client/views/auth/auth.js
+++ client/views/auth/auth.js
@@ -2,51 +2,50 @@ Template.login.onCreated(function() {
   this.error = new ReactiveVar(false);
 });
 
-
 Template.login.helpers({
-  loading: () => Meteor.loggingIn() && 'loading',
-  hasError: () => Template.instance().error.get() ? 'error' : false,
+  loading: () => Meteor.loggingIn() && "loading",
+  hasError: () => (Template.instance().error.get() ? "error" : false),
   errorMessage: () => Template.instance().error.get(),
-  configuredServices: () => _.pluck(Accounts.loginServiceConfiguration.find().fetch(), "service"),
-  isEnabled: (service) => Accounts.loginServiceConfiguration.findOne({ service: service }),
-  loginMessage: () => Meteor.settings.public.loginMessage,
+  configuredServices: () =>
+    _.pluck(Accounts.loginServiceConfiguration.find().fetch(), "service"),
+  isEnabled: service =>
+    Accounts.loginServiceConfiguration.findOne({ service: service }),
+  loginMessage: () => Meteor.settings.public.loginMessage
 });
 
-
 Template.login.events({
-  'submit form': function(event, template) {
+  "submit form": function(event, template) {
     event.preventDefault();
 
-    var username = template.find('[name=username]').value;
-    var password = template.find('[name=password]').value;
+    var username = template.find("[name=username]").value;
+    var password = template.find("[name=password]").value;
 
-    Meteor.loginWithPassword(username, password, function(err) {
+    Meteor.loginWithPassword({ username }, password, function(err) {
       if (err) {
         if (Meteor.settings.public.ldapEnabled) {
           Meteor.loginWithLDAP(username, password, function(err) {
             if (err) template.error.set(err.reason);
-          });  
-        }
-        else {
+          });
+        } else {
           template.error.set(err.reason);
         }
       }
     });
   },
 
-  'click .google.button': function(event, template) {
+  "click .google.button": function(event, template) {
     Meteor.loginWithGoogle({}, function(err) {
       if (err) template.error.set(err.reason);
     });
   },
 
-  'click .github.button': function(event, template) {
+  "click .github.button": function(event, template) {
     Meteor.loginWithGithub({}, function(err) {
       if (err) template.error.set(err.reason);
     });
   },
 
-  'click .facebook.button': function(event, template) {
+  "click .facebook.button": function(event, template) {
     Meteor.loginWithFacebook({}, function(err) {
       if (err) template.error.set(err.reason);
     });
