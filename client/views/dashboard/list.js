2554bf111bc1e7c1380ca22f1b121f70ef924489
Sven <sven.perkmann@gmail.com>
2019-01-06 02:24:31 +0100

    sorting dashboards alphabetically

--- client/views/dashboard/list.js
+++ client/views/dashboard/list.js
@@ -1,29 +1,27 @@
 loadHandler(Template.dashboardList);
 
-
 function dashboardFilter() {
   var selector = {};
 
-  if (FlowRouter.getQueryParam('search'))
-    selector['$or'] = [
-      { title: { $regex: FlowRouter.getQueryParam('search'), $options: 'i' } },
-      { owner: { $regex: FlowRouter.getQueryParam('search'), $options: 'i' } },
-      { tags: FlowRouter.getQueryParam('search') }
+  if (FlowRouter.getQueryParam("search"))
+    selector["$or"] = [
+      { title: { $regex: FlowRouter.getQueryParam("search"), $options: "i" } },
+      { owner: { $regex: FlowRouter.getQueryParam("search"), $options: "i" } },
+      { tags: FlowRouter.getQueryParam("search") }
     ];
-  if (FlowRouter.getQueryParam('filterOwn') === 'true')
-    selector['ownerId'] = Meteor.userId();
-  if (FlowRouter.getQueryParam('tag'))
-    selector['tags'] = FlowRouter.getQueryParam('tag');
-  
+  if (FlowRouter.getQueryParam("filterOwn") === "true")
+    selector["ownerId"] = Meteor.userId();
+  if (FlowRouter.getQueryParam("tag"))
+    selector["tags"] = FlowRouter.getQueryParam("tag");
+
   return selector;
 }
-
 
 Template.dashboardList.onCreated(function() {
   this.limit = new ReactiveVar();
   this.tags = new ReactiveVar([]);
 
-  this.subscribe('favoriteDashboards');
+  this.subscribe("favoriteDashboards");
 
   this.autorun(() => {
     dashboardFilter();
@@ -31,57 +29,62 @@ Template.dashboardList.onCreated(function() {
   });
 
   this.autorun(() => {
-    this.subscribe('dashboards', dashboardFilter(), this.limit.get());
+    this.subscribe("dashboards", dashboardFilter(), this.limit.get());
   });
 
-  Meteor.call('getDashboardTags', (err, res) => this.tags.set(res));
+  Meteor.call("getDashboardTags", (err, res) => this.tags.set(res));
 });
-
 
 Template.dashboardList.onRendered(function() {
   var template = this;
 
-  template.$('.ui.checkbox').checkbox();
+  template.$(".ui.checkbox").checkbox();
 
-  template.$('.ui.all.cards').visibility({
+  template.$(".ui.all.cards").visibility({
     once: false,
     observeChanges: true,
     onBottomVisible() {
       if (template.subscriptionsReady()) {
-        template.$('.js-load-more').click();
-      } 
+        template.$(".js-load-more").click();
+      }
     }
   });
 });
 
-
 Template.dashboardList.helpers({
-  dashboards: () => Dashboards.find(dashboardFilter()),
-  starredDashboards: () => Dashboards.find(_.extend(dashboardFilter(), {
-    _id: { $in: getStarred('dashboard', Meteor.user()) }
-  })),
+  dashboards: () => Dashboards.find(dashboardFilter(), { sort: { title: 1 } }),
+  starredDashboards: () =>
+    Dashboards.find({
+      ...dashboardFilter(),
+      _id: { $in: getStarred("dashboard", Meteor.user()) }
+    }),
 
   tags: () => Template.instance().tags.get(),
-  search: () => FlowRouter.getQueryParam('search'),
-  filterOwn: () => FlowRouter.getQueryParam('filterOwn') === 'true',
+  search: () => FlowRouter.getQueryParam("search"),
+  filterOwn: () => FlowRouter.getQueryParam("filterOwn") === "true",
 
-  tagLabelClass: (tag) => (FlowRouter.getQueryParam('tag') == tag) ? 'basic blue' : 'basic',
+  tagLabelClass: tag =>
+    FlowRouter.getQueryParam("tag") == tag ? "basic blue" : "basic",
 
   hasMore: function(items) {
-    return !Template.instance().subscriptionsReady() || 
-      items.count() >= Template.instance().limit.get();
-  },
+    return (
+      !Template.instance().subscriptionsReady() ||
+      items.count() >= Template.instance().limit.get()
+    );
+  }
 });
-
 
 Template.dashboardList.events({
-  'keyup input[name=search], change input[name=search]': (event) => FlowRouter.setQueryParams({ search: event.target.value || null }),
-  'change input[name=filterOwn]': (event) => FlowRouter.setQueryParams({ filterOwn: event.target.checked || null }),
-  'click .js-select-tag': (event, template) => FlowRouter.setQueryParams({ tag: event.target.dataset.value || null }),
-  'click .js-load-more': (event, template) => template.limit.set(template.limit.get() + 10),
+  "keyup input[name=search], change input[name=search]": event =>
+    FlowRouter.setQueryParams({ search: event.target.value || null }),
+  "change input[name=filterOwn]": event =>
+    FlowRouter.setQueryParams({ filterOwn: event.target.checked || null }),
+  "click .js-select-tag": (event, template) =>
+    FlowRouter.setQueryParams({ tag: event.target.dataset.value || null }),
+  "click .js-load-more": (event, template) =>
+    template.limit.set(template.limit.get() + 10)
 });
 
-
 Template.dashboardListItem.helpers({
-  hasStarred: (dashboard) => hasStarred('dashboard', dashboard._id)
+  hasStarred: dashboard => hasStarred("dashboard", dashboard._id)
 });
