1f9cf484d38862e96e24dfea594c288543fdd6d1
Sven <sven.perkmann@gmail.com>
2019-08-21 18:26:07 +0200

    FIXES bigNumber number parsing

--- client/views/visualization/widgets/bigNumber/bigNumber.js
+++ client/views/visualization/widgets/bigNumber/bigNumber.js
@@ -1,23 +1,27 @@
-import { format as d3Format } from 'd3';
-
+import { format as d3Format } from "d3";
 
 Template.visBigNumber.helpers({
   value: function() {
     var settings = this.settings;
-    settings.valueFormat = settings.valueFormat || ',';
+    settings.valueFormat = settings.valueFormat || ",";
 
     if (settings.valueField && settings.valueAggregation) {
       var aggregationFunction = AGGREGATIONS[settings.valueAggregation].fn;
       var values = _.pluck(this.data, settings.valueField);
       var result = aggregationFunction(values);
 
-      return _.isNumber(result) ? d3Format(settings.valueFormat)(result) : result;
-    }
-    else return '-';
-  },
+      var parsedResult = parseFloat(result);
+      if (!isNaN(parsedResult)) {
+        result = parsedResult;
+      }
+
+      return _.isNumber(result)
+        ? d3Format(settings.valueFormat)(result)
+        : result;
+    } else return "-";
+  }
 });
 
-
 Template.visBigNumberForm.helpers({
-  aggregationMethods: _.keys(AGGREGATIONS),
+  aggregationMethods: _.keys(AGGREGATIONS)
 });
